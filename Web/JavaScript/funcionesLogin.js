2d7e8ffd66b8b5c8392d23865c97019854c20393
EmmanuelTorresVaz <emmanuel.torresvaz@gmail.com>
2020-01-19 10:01:06 -0600

    Menu inicio y login

---
+++ Web/JavaScript/funcionesLogin.js
@@ -0,0 +1,32 @@
+function validarUser() {
+    var user = $("#usuario").val();
+    var pass = $("#password").val();
+    var validate = true;
+    if(user==''||user==null){
+        alertify.error("Por favor ingrese un usuario.");
+        validate=false;
+    }
+    if(pass==''||pass==null){
+        alertify.error("Por favor ingrese una contraseña.");
+        validate=false;
+    }
+    if (validate) {
+        var dataEnviar = {
+            "User": user,
+            "Pass": pass,
+        };
+        $.ajax({
+            type: "POST",
+            url: '../../../com.Mexicash/Controlador/contLogin.php',
+            data: dataEnviar,
+            success: function (response) {
+                if(response>0){
+                    location.href='../Empeno/vInicio.php'
+                }else{
+                    $('#resultado').html("Verifique usuario y contraseña.");
+                }
+            }
+
+        });
+    }
+}
